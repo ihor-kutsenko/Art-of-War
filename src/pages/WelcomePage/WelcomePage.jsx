ae8150762cf58651230b31a704c310574d936cc5
Rammbest <igor-kutsenko@ukr.net>
2024-04-09 15:48:13 +0300

    add main img

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,12 @@
+import main from '../../images/main.jpg';
 import styles from './WelcomePage.module.scss';
 
 const WelcomePage = () => {
-  return <div className={styles.title}>Welcome Page</div>;
+  return (
+    <div className={styles.title}>
+      <img src={main} alt="art of war" />
+    </div>
+  );
 };
 
 export default WelcomePage;
