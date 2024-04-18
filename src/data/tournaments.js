bedc5a943749e43f192d950623d147b2a323bdc0
Rammbest <igor-kutsenko@ukr.net>
2024-04-18 23:08:24 +0300

    fix error

--- src/data/tournaments.js
+++ src/data/tournaments.js
@@ -2,9 +2,9 @@ import nautilusImage from '../images/tournaments/nautilus.jpg';
 import { nautilusMedals } from './tournaments/nautilus';
 
 import vanguardImage from '../images/tournaments/vanguard.jpg';
-// import { vanguardMedals } from './tournaments/vanguard';
+import { vanguardMedals } from './tournaments/vanguard';
 
-// import battleRoyaleImage from '../images/tournaments/battleRoyale/battle-royale.jpg';
+import battleRoyaleImage from '../images/tournaments/battle-royale.jpg';
 
 export const tournaments = [
   {
@@ -17,12 +17,12 @@ export const tournaments = [
     title: 'Vanguard',
     imageUrl: vanguardImage,
     buttonText: 'Medals',
+    medals: vanguardMedals,
+  },
+  {
+    title: 'Battle Royale',
+    imageUrl: battleRoyaleImage,
+    buttonText: 'Medals',
     medals: nautilusMedals,
   },
-  // {
-  //   title: 'Battle Royale',
-  //   imageUrl: battleRoyaleImage,
-  //   buttonText: 'Medals',
-  //   medals: nautilusMedals,
-  // },
 ];
