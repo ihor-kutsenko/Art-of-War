f45dea4026ca92b11fb3d76d22d04bcd5f0f27fd
Rammbest <igor-kutsenko@ukr.net>
2024-04-18 23:02:57 +0300

    Update tournaments.js

--- src/data/tournaments.js
+++ src/data/tournaments.js
@@ -1,7 +1,7 @@
 import nautilusImage from '../images/tournaments/nautilus/nautilus.jpg';
 import { nautilusMedals } from './tournaments/nautilus';
 
-// import vanguardImage from '../images/tournaments/vanguard/vanguard.jpg';
+import vanguardImage from '../images/tournaments/vanguard/vanguard.jpg';
 // import { vanguardMedals } from './tournaments/vanguard';
 
 // import battleRoyaleImage from '../images/tournaments/battleRoyale/battle-royale.jpg';
@@ -13,12 +13,12 @@ export const tournaments = [
     buttonText: 'Medals',
     medals: nautilusMedals,
   },
-  // {
-  //   title: 'Vanguard',
-  //   imageUrl: vanguardImage,
-  //   buttonText: 'Medals',
-  //   medals: vanguardMedals,
-  // },
+  {
+    title: 'Vanguard',
+    imageUrl: vanguardImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
   // {
   //   title: 'Battle Royale',
   //   imageUrl: battleRoyaleImage,
