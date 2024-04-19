cb7bf73dd64e585b97f03c03fbd3d6ccd9422933
Rammbest <igor-kutsenko@ukr.net>
2024-04-19 21:51:20 +0300

    add tournamets data

--- src/data/tournaments.js
+++ src/data/tournaments.js
@@ -5,10 +5,28 @@ import vanguardImage from '../images/tournaments/vanguard.jpg';
 import { vanguardMedals } from './tournaments/vanguard';
 
 import battleRoyaleImage from '../images/tournaments/battle-royale.jpg';
+import fifthAnniversaryImage from '../images/tournaments/5th-anniversary.jpg';
+import allForTheFrontImage from '../images/tournaments/all-for-the-front.jpg';
+import assaultImage from '../images/tournaments/assault.jpg';
+import newYearImage from '../images/tournaments/battle-for-christmas-trees.jpg';
+import behindTheWallImage from '../images/tournaments/behind-the-wall.jpg';
+import niceLootImage from '../images/tournaments/big-booty.jpg';
+import corsairsImage from '../images/tournaments/corsairs.jpg';
+import crossfireImage from '../images/tournaments/crossfire.jpg';
+import flightRecorderImage from '../images/tournaments/flight-recoder.png';
+import jokerImage from '../images/tournaments/joker.jpg';
+import judgmentDayImage from '../images/tournaments/judgment-day.jpg';
+import largeReservesImage from '../images/tournaments/large-reserves.jpg';
+import onlyByChanceImage from '../images/tournaments/only-by-chance.jpg';
+import pumpkinMadnessImage from '../images/tournaments/pumpkin-madness.jpg';
+import reliablePartnerImage from '../images/tournaments/reliable-partner.jpg';
+import totalControlImage from '../images/tournaments/total-control.jpg';
+import twoCitadelsImage from '../images/tournaments/two-citadels.jpg';
+import birthdayImage from '../images/tournaments/birthday.jpg';
 
 export const tournaments = [
   {
-    title: 'Nautilus search',
+    title: 'Searching the Nautilus ',
     imageUrl: nautilusImage,
     buttonText: 'Medals',
     medals: nautilusMedals,
@@ -22,6 +40,114 @@ export const tournaments = [
   {
     title: 'Battle Royale',
     imageUrl: battleRoyaleImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: '5th Anniversary',
+    imageUrl: fifthAnniversaryImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'All for the Front',
+    imageUrl: allForTheFrontImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Assault',
+    imageUrl: assaultImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'New Years Alarm',
+    imageUrl: newYearImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Behind the Wall',
+    imageUrl: behindTheWallImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Nice Loot',
+    imageUrl: niceLootImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Corsairs',
+    imageUrl: corsairsImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Crossfire',
+    imageUrl: crossfireImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Flight Recorder',
+    imageUrl: flightRecorderImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Joker',
+    imageUrl: jokerImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Judgment Day',
+    imageUrl: judgmentDayImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Large Reserves',
+    imageUrl: largeReservesImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Only by Chance',
+    imageUrl: onlyByChanceImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Pumpkin Madness',
+    imageUrl: pumpkinMadnessImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Reliable Partner',
+    imageUrl: reliablePartnerImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Total Control',
+    imageUrl: totalControlImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Two Citadels',
+    imageUrl: twoCitadelsImage,
+    buttonText: 'Medals',
+    medals: nautilusMedals,
+  },
+  {
+    title: 'Game Birthday',
+    imageUrl: birthdayImage,
     buttonText: 'Medals',
     medals: nautilusMedals,
   },
