983a27b811722b14799d576a39f8447bda28d159
Rammbest <igor-kutsenko@ukr.net>
2024-05-01 21:45:45 +0300

    add styles to orders

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,17 +3,14 @@ import { orders } from 'data/orders';
 
 const Orders = () => {
   return (
-    <>
+    <div className={styles.container}>
       {orders.map((order, index) => (
         <div key={index} className={styles.item}>
-          <div className={styles.container}>
-            <h3 className={styles.title}>{order.name}</h3>
-            <img src={order.image} alt={order.name} className={styles.img} />
-          </div>
+          <h3 className={styles.title}>{order.name}</h3>
+          <img src={order.image} alt={order.name} className={styles.img} />
         </div>
       ))}
-      ;
-    </>
+    </div>
   );
 };
 
