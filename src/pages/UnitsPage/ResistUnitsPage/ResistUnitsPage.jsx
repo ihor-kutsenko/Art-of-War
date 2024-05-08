58ceadcced5268da2abc7b2546ed83927e8578d2
Rammbest <igor-kutsenko@ukr.net>
2024-05-09 00:41:06 +0300

    add confederations avatars

--- src/pages/UnitsPage/ResistUnitsPage/ResistUnitsPage.jsx
+++ src/pages/UnitsPage/ResistUnitsPage/ResistUnitsPage.jsx
@@ -1,7 +1,14 @@
+import Avatars from 'components/Avatars/Avatars';
+import { avatarsConfederations } from 'data/avatars/confederation';
 import React from 'react';
 
 const ResistUnitsPage = () => {
-  return <div>ResistancePage</div>;
+  return (
+    <div>
+      ResistancePage
+      <Avatars avatars={avatarsConfederations} />
+    </div>
+  );
 };
 
 export default ResistUnitsPage;
