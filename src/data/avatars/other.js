00321d80b52fafa67d935197629bb09cd84ab590
Rammbest <igor-kutsenko@ukr.net>
2024-05-11 20:34:48 +0300

    add data avtars.other

---
+++ src/data/avatars/other.js
@@ -0,0 +1,21 @@
+import avatar1 from '../../images/avatars/other/1.png';
+import avatar2 from '../../images/avatars/other/2.png';
+import avatar3 from '../../images/avatars/other/3.png';
+import avatar4 from '../../images/avatars/other/4.png';
+import avatar5 from '../../images/avatars/other/5.png';
+import avatar6 from '../../images/avatars/other/6.png';
+import avatar7 from '../../images/avatars/other/7.png';
+import avatar8 from '../../images/avatars/other/8.png';
+import avatar9 from '../../images/avatars/other/9.png';
+
+export const avatarsOther = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9,
+];
