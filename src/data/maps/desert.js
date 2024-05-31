225986b086a19bbd944cc04353b56a631a4c5229
Rammbest <igor-kutsenko@ukr.net>
2024-06-01 00:12:36 +0300

    add desert maps data and page

---
+++ src/data/maps/desert.js
@@ -0,0 +1,53 @@
+import areaOfScavengers from '../../images/maps/desert/area-of-scavengers.jpg';
+import aWrongWayTurn from '../../images/maps/desert/a-wrong-way-turn.jpg';
+import bloodyShore from '../../images/maps/desert/bloody-shore.jpg';
+import caesarsIslands from '../../images/maps/desert/caesars-islands.jpg';
+import coralBay from '../../images/maps/desert/coral-bay.jpg';
+import deadlyCliff from '../../images/maps/desert/deadly-cliff.jpg';
+import desertCross from '../../images/maps/desert/desert-cross.jpg';
+import desertTreasure from '../../images/maps/desert/desert-treasure.jpg';
+import difficultChoice from '../../images/maps/desert/difficult-choice.jpg';
+import grandCanyon from '../../images/maps/desert/grand-canyon.jpg';
+import inferno from '../../images/maps/desert/inferno.jpg';
+import mountainRoad from '../../images/maps/desert/mountain-road.jpg';
+import noTrace from '../../images/maps/desert/no-trace.jpg';
+import sandyBay from '../../images/maps/desert/sandy-bay.jpg';
+import simoom from '../../images/maps/desert/simoom.jpg';
+import suezCanal from '../../images/maps/desert/suez-canal.jpg';
+import theAbandonedOasis from '../../images/maps/desert/the-abandoned-oasis.jpg';
+import theCanyon from '../../images/maps/desert/the-canyon.jpg';
+import thePass from '../../images/maps/desert/the-pass.jpg';
+import theSilkRoad from '../../images/maps/desert/the-silk-road.jpg';
+import thornyPath from '../../images/maps/desert/thorny-path.jpg';
+import twoMountains from '../../images/maps/desert/two-mountains.jpg';
+import whirlwind from '../../images/maps/desert/whirlwind.jpg';
+import yawaraLake from '../../images/maps/desert/yawara-lake.jpg';
+import yrinwyReservoir from '../../images/maps/desert/yrinwy-reservoir.jpg';
+
+export const desertMaps = [
+  { title: 'Area Of Scavengers', image: areaOfScavengers },
+  { title: 'A Wrong Way Turn', image: aWrongWayTurn },
+  { title: 'Bloody Shore', image: bloodyShore },
+  { title: 'Caesars Islands', image: caesarsIslands },
+  { title: 'Coral Bay', image: coralBay },
+  { title: 'Deadly Cliff', image: deadlyCliff },
+  { title: 'Desert Cross', image: desertCross },
+  { title: 'Desert Treasure', image: desertTreasure },
+  { title: 'Difficult Choice', image: difficultChoice },
+  { title: 'Grand Canyon', image: grandCanyon },
+  { title: 'Inferno', image: inferno },
+  { title: 'Mountain Road', image: mountainRoad },
+  { title: 'No Trace', image: noTrace },
+  { title: 'Sandy Bay', image: sandyBay },
+  { title: 'Simoom', image: simoom },
+  { title: 'Suez Canal', image: suezCanal },
+  { title: 'The Abandoned Oasis', image: theAbandonedOasis },
+  { title: 'The Canyon', image: theCanyon },
+  { title: 'The Pass', image: thePass },
+  { title: 'The Silk Road', image: theSilkRoad },
+  { title: 'Thorny Path', image: thornyPath },
+  { title: 'Two Mountains', image: twoMountains },
+  { title: 'Whirlwind', image: whirlwind },
+  { title: 'Yawara Lake', image: yawaraLake },
+  { title: 'Yrinwy Reservoir', image: yrinwyReservoir },
+];
--- src/pages/MapsPage/DesertPage/DesertPage.jsx
+++ src/pages/MapsPage/DesertPage/DesertPage.jsx
@@ -1,7 +1,13 @@
+import Gallery from 'components/Gallery/Gallery';
+import { desertMaps } from 'data/maps/desert';
 import React from 'react';
 
 const DesertPage = () => {
-  return <div>Desert maps</div>;
+  return (
+    <>
+      <Gallery images={desertMaps} />
+    </>
+  );
 };
 
 export default DesertPage;
