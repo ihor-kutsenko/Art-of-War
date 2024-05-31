a0c673f4959b7d030e4a07cf8e966ac94ce4a9df
Rammbest <igor-kutsenko@ukr.net>
2024-05-31 12:07:02 +0300

    add new autumn maps

--- src/data/maps/autumn.js
+++ src/data/maps/autumn.js
@@ -2,10 +2,14 @@ import area13 from '../../images/maps/autumn/area-13.jpg';
 import industrialCanal from '../../images/maps/autumn/industrial-canal.jpg';
 import ironCurtain from '../../images/maps/autumn/iron-curtain.jpg';
 import ravensHill from '../../images/maps/autumn/ravens-hill.jpg';
+import goldenRing from '../../images/maps/autumn/golden-ring.jpg';
+import steelGrip from '../../images/maps/autumn/steel-grip.jpg';
 
 export const autumnMaps = [
   { title: 'Area 13', image: area13 },
   { title: 'Industrial Canal', image: industrialCanal },
   { title: 'Iron Curtain', image: ironCurtain },
   { title: 'Ravens Hill', image: ravensHill },
+  { title: 'Golden Ring', image: goldenRing },
+  { title: 'Steel Grip', image: steelGrip },
 ];
