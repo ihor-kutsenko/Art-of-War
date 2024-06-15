1503efba7baafdb31d5f63ad384cfc9590c5292e
ihor-kutsenko <96702664+ihor-kutsenko@users.noreply.github.com>
2024-06-15 22:26:56 +0000

    Deploying to gh-pages from @ ihor-kutsenko/Art-of-War@9ec52359de47508e3ac3397d6e6a7fb89d1a733a 🚀

---
+++ static/js/122.2e84ae59.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkart_of_war=self.webpackChunkart_of_war||[]).push([[122],{82122:function(r,n,a){a.r(n);a(72791);var e=a(80184);n.default=function(){return(0,e.jsx)("div",{children:"conf"})}}}]);
+//# sourceMappingURL=122.2e84ae59.chunk.js.map
