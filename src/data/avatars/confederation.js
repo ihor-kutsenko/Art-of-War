146b6ee74400b7b49031ca63397b33fbf8c27319
Rammbest <igor-kutsenko@ukr.net>
2024-09-25 21:36:38 +0300

    transfer of avatars to cloudinary

--- src/data/avatars/confederation.js
+++ src/data/avatars/confederation.js
@@ -1,71 +1,105 @@
-import avatar1 from '../../images/avatars/confederation/1.png';
-import avatar2 from '../../images/avatars/confederation/2.png';
-import avatar3 from '../../images/avatars/confederation/3.png';
-import avatar4 from '../../images/avatars/confederation/4.png';
-import avatar5 from '../../images/avatars/confederation/5.png';
-import avatar6 from '../../images/avatars/confederation/6.png';
-import avatar7 from '../../images/avatars/confederation/7.png';
-import avatar8 from '../../images/avatars/confederation/8.png';
-import avatar9 from '../../images/avatars/confederation/9.png';
-import avatar10 from '../../images/avatars/confederation/10.png';
-import avatar11 from '../../images/avatars/confederation/11.png';
-import avatar12 from '../../images/avatars/confederation/12.png';
-import avatar13 from '../../images/avatars/confederation/13.png';
-import avatar14 from '../../images/avatars/confederation/14.png';
-import avatar15 from '../../images/avatars/confederation/15.png';
-import avatar16 from '../../images/avatars/confederation/16.png';
-import avatar17 from '../../images/avatars/confederation/17.png';
-import avatar18 from '../../images/avatars/confederation/18.png';
-import avatar19 from '../../images/avatars/confederation/19.png';
-import avatar20 from '../../images/avatars/confederation/20.png';
-import avatar21 from '../../images/avatars/confederation/21.png';
-import avatar22 from '../../images/avatars/confederation/22.png';
-import avatar23 from '../../images/avatars/confederation/23.png';
-import avatar24 from '../../images/avatars/confederation/24.png';
-import avatar25 from '../../images/avatars/confederation/25.png';
-import avatar26 from '../../images/avatars/confederation/26.png';
-import avatar27 from '../../images/avatars/confederation/27.png';
-import avatar28 from '../../images/avatars/confederation/28.png';
-import avatar29 from '../../images/avatars/confederation/29.png';
-import avatar30 from '../../images/avatars/confederation/30.png';
-import avatar31 from '../../images/avatars/confederation/31.png';
-import avatar32 from '../../images/avatars/confederation/32.png';
-import avatar33 from '../../images/avatars/confederation/33.png';
-import avatar34 from '../../images/avatars/confederation/34.png';
+// import avatar1 from '../../images/avatars/confederation/1.png';
+// import avatar2 from '../../images/avatars/confederation/2.png';
+// import avatar3 from '../../images/avatars/confederation/3.png';
+// import avatar4 from '../../images/avatars/confederation/4.png';
+// import avatar5 from '../../images/avatars/confederation/5.png';
+// import avatar6 from '../../images/avatars/confederation/6.png';
+// import avatar7 from '../../images/avatars/confederation/7.png';
+// import avatar8 from '../../images/avatars/confederation/8.png';
+// import avatar9 from '../../images/avatars/confederation/9.png';
+// import avatar10 from '../../images/avatars/confederation/10.png';
+// import avatar11 from '../../images/avatars/confederation/11.png';
+// import avatar12 from '../../images/avatars/confederation/12.png';
+// import avatar13 from '../../images/avatars/confederation/13.png';
+// import avatar14 from '../../images/avatars/confederation/14.png';
+// import avatar15 from '../../images/avatars/confederation/15.png';
+// import avatar16 from '../../images/avatars/confederation/16.png';
+// import avatar17 from '../../images/avatars/confederation/17.png';
+// import avatar18 from '../../images/avatars/confederation/18.png';
+// import avatar19 from '../../images/avatars/confederation/19.png';
+// import avatar20 from '../../images/avatars/confederation/20.png';
+// import avatar21 from '../../images/avatars/confederation/21.png';
+// import avatar22 from '../../images/avatars/confederation/22.png';
+// import avatar23 from '../../images/avatars/confederation/23.png';
+// import avatar24 from '../../images/avatars/confederation/24.png';
+// import avatar25 from '../../images/avatars/confederation/25.png';
+// import avatar26 from '../../images/avatars/confederation/26.png';
+// import avatar27 from '../../images/avatars/confederation/27.png';
+// import avatar28 from '../../images/avatars/confederation/28.png';
+// import avatar29 from '../../images/avatars/confederation/29.png';
+// import avatar30 from '../../images/avatars/confederation/30.png';
+// import avatar31 from '../../images/avatars/confederation/31.png';
+// import avatar32 from '../../images/avatars/confederation/32.png';
+// import avatar33 from '../../images/avatars/confederation/33.png';
+// import avatar34 from '../../images/avatars/confederation/34.png';
 
 export const avatarsConfederations = [
-  avatar1,
-  avatar2,
-  avatar3,
-  avatar4,
-  avatar5,
-  avatar6,
-  avatar7,
-  avatar8,
-  avatar9,
-  avatar10,
-  avatar11,
-  avatar12,
-  avatar13,
-  avatar14,
-  avatar15,
-  avatar16,
-  avatar17,
-  avatar18,
-  avatar19,
-  avatar20,
-  avatar21,
-  avatar22,
-  avatar23,
-  avatar24,
-  avatar25,
-  avatar26,
-  avatar27,
-  avatar28,
-  avatar29,
-  avatar30,
-  avatar31,
-  avatar32,
-  avatar33,
-  avatar34,
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288659/1_vuphkf.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288628/2_rnbecg.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288628/3_zd1tzr.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288628/4_drg88t.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288629/5_dzf3n8.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288630/6_gmi5nw.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288631/7_kl0bcb.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288631/8_vvheen.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288633/9_uxsoth.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288632/10_hrqzuj.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288634/11_u3akqy.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288635/12_ewistz.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288635/13_sjxzft.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288637/14_epud8i.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288637/15_ojuid6.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288639/16_jaeqvc.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288641/17_khdlga.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288639/18_o4khpf.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288642/19_fbcdhx.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288644/20_lyfbuk.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288644/21_odliso.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288645/22_plpjj9.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288647/23_bfqlkm.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288648/24_qruaqp.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288649/25_ukfyuq.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288651/26_vtsp9p.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288651/27_amw2tc.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288652/28_kfkru4.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288653/29_hu9nhg.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288655/30_mn8yui.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288655/31_tq0nwn.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288656/32_da4ff2.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288658/33_pigcpe.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727288658/34_rtv0zk.webp',
+  // avatar1,
+  // avatar2,
+  // avatar3,
+  // avatar4,
+  // avatar5,
+  // avatar6,
+  // avatar7,
+  // avatar8,
+  // avatar9,
+  // avatar10,
+  // avatar11,
+  // avatar12,
+  // avatar13,
+  // avatar14,
+  // avatar15,
+  // avatar16,
+  // avatar17,
+  // avatar18,
+  // avatar19,
+  // avatar20,
+  // avatar21,
+  // avatar22,
+  // avatar23,
+  // avatar24,
+  // avatar25,
+  // avatar26,
+  // avatar27,
+  // avatar28,
+  // avatar29,
+  // avatar30,
+  // avatar31,
+  // avatar32,
+  // avatar33,
+  // avatar34,
 ];
--- src/data/avatars/resistance.js
+++ src/data/avatars/resistance.js
@@ -1,71 +1,106 @@
-import avatar1 from '../../images/avatars/resistance/1.png';
-import avatar2 from '../../images/avatars/resistance/2.png';
-import avatar3 from '../../images/avatars/resistance/3.png';
-import avatar4 from '../../images/avatars/resistance/4.png';
-import avatar5 from '../../images/avatars/resistance/5.png';
-import avatar6 from '../../images/avatars/resistance/6.png';
-import avatar7 from '../../images/avatars/resistance/7.png';
-import avatar8 from '../../images/avatars/resistance/8.png';
-import avatar9 from '../../images/avatars/resistance/9.png';
-import avatar10 from '../../images/avatars/resistance/10.png';
-import avatar11 from '../../images/avatars/resistance/11.png';
-import avatar12 from '../../images/avatars/resistance/12.png';
-import avatar13 from '../../images/avatars/resistance/13.png';
-import avatar14 from '../../images/avatars/resistance/14.png';
-import avatar15 from '../../images/avatars/resistance/15.png';
-import avatar16 from '../../images/avatars/resistance/16.png';
-import avatar17 from '../../images/avatars/resistance/17.png';
-import avatar18 from '../../images/avatars/resistance/18.png';
-import avatar19 from '../../images/avatars/resistance/19.png';
-import avatar20 from '../../images/avatars/resistance/20.png';
-import avatar21 from '../../images/avatars/resistance/21.png';
-import avatar22 from '../../images/avatars/resistance/22.png';
-import avatar23 from '../../images/avatars/resistance/23.png';
-import avatar24 from '../../images/avatars/resistance/24.png';
-import avatar25 from '../../images/avatars/resistance/25.png';
-import avatar26 from '../../images/avatars/resistance/26.png';
-import avatar27 from '../../images/avatars/resistance/27.png';
-import avatar28 from '../../images/avatars/resistance/28.png';
-import avatar29 from '../../images/avatars/resistance/29.png';
-import avatar30 from '../../images/avatars/resistance/30.png';
-import avatar31 from '../../images/avatars/resistance/31.png';
-import avatar32 from '../../images/avatars/resistance/32.png';
-import avatar33 from '../../images/avatars/resistance/33.png';
-import avatar34 from '../../images/avatars/resistance/34.png';
+// import avatar1 from '../../images/avatars/resistance/1.png';
+// import avatar2 from '../../images/avatars/resistance/2.png';
+// import avatar3 from '../../images/avatars/resistance/3.png';
+// import avatar4 from '../../images/avatars/resistance/4.png';
+// import avatar5 from '../../images/avatars/resistance/5.png';
+// import avatar6 from '../../images/avatars/resistance/6.png';
+// import avatar7 from '../../images/avatars/resistance/7.png';
+// import avatar8 from '../../images/avatars/resistance/8.png';
+// import avatar9 from '../../images/avatars/resistance/9.png';
+// import avatar10 from '../../images/avatars/resistance/10.png';
+// import avatar11 from '../../images/avatars/resistance/11.png';
+// import avatar12 from '../../images/avatars/resistance/12.png';
+// import avatar13 from '../../images/avatars/resistance/13.png';
+// import avatar14 from '../../images/avatars/resistance/14.png';
+// import avatar15 from '../../images/avatars/resistance/15.png';
+// import avatar16 from '../../images/avatars/resistance/16.png';
+// import avatar17 from '../../images/avatars/resistance/17.png';
+// import avatar18 from '../../images/avatars/resistance/18.png';
+// import avatar19 from '../../images/avatars/resistance/19.png';
+// import avatar20 from '../../images/avatars/resistance/20.png';
+// import avatar21 from '../../images/avatars/resistance/21.png';
+// import avatar22 from '../../images/avatars/resistance/22.png';
+// import avatar23 from '../../images/avatars/resistance/23.png';
+// import avatar24 from '../../images/avatars/resistance/24.png';
+// import avatar25 from '../../images/avatars/resistance/25.png';
+// import avatar26 from '../../images/avatars/resistance/26.png';
+// import avatar27 from '../../images/avatars/resistance/27.png';
+// import avatar28 from '../../images/avatars/resistance/28.png';
+// import avatar29 from '../../images/avatars/resistance/29.png';
+// import avatar30 from '../../images/avatars/resistance/30.png';
+// import avatar31 from '../../images/avatars/resistance/31.png';
+// import avatar32 from '../../images/avatars/resistance/32.png';
+// import avatar33 from '../../images/avatars/resistance/33.png';
+// import avatar34 from '../../images/avatars/resistance/34.png';
 
 export const avatarsResistance = [
-  avatar1,
-  avatar2,
-  avatar3,
-  avatar4,
-  avatar5,
-  avatar6,
-  avatar7,
-  avatar8,
-  avatar9,
-  avatar10,
-  avatar11,
-  avatar12,
-  avatar13,
-  avatar14,
-  avatar15,
-  avatar16,
-  avatar17,
-  avatar18,
-  avatar19,
-  avatar20,
-  avatar21,
-  avatar22,
-  avatar23,
-  avatar24,
-  avatar25,
-  avatar26,
-  avatar27,
-  avatar28,
-  avatar29,
-  avatar30,
-  avatar31,
-  avatar32,
-  avatar33,
-  avatar34,
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287499/1_flmack.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287499/2_n3sosl.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287500/3_xw09zp.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287485/4_cf5mo9.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287486/5_n92dwy.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287485/6_tydepv.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287486/7_esqwzy.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287486/8_ignuvg.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287486/9_lx9jwi.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287486/10_noqwcn.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287486/11_qsnxi8.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287487/12_cklfaz.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287487/13_pls8r4.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287487/14_bl29cq.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287487/15_kuvrzi.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287487/16_m3hbci.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287488/17_ulcvos.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287490/18_pfvfku.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287491/19_zh3xyd.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287490/20_zzpaih.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287491/21_izejvb.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287491/22_nkjibx.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287491/23_d579a6.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287492/24_ymfmuw.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287493/25_m8jq9c.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287494/26_xrcpfe.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287494/27_q3kar7.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287495/28_zn4o0w.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287496/29_ulfaxw.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287496/30_lq3f8t.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287603/31_zywcfv.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287496/32_gypvm1.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287497/33_ftbc2w.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727287497/34_hnwhnd.webp',
+
+  // avatar1,
+  // avatar2,
+  // avatar3,
+  // avatar4,
+  // avatar5,
+  // avatar6,
+  // avatar7,
+  // avatar8,
+  // avatar9,
+  // avatar10,
+  // avatar11,
+  // avatar12,
+  // avatar13,
+  // avatar14,
+  // avatar15,
+  // avatar16,
+  // avatar17,
+  // avatar18,
+  // avatar19,
+  // avatar20,
+  // avatar21,
+  // avatar22,
+  // avatar23,
+  // avatar24,
+  // avatar25,
+  // avatar26,
+  // avatar27,
+  // avatar28,
+  // avatar29,
+  // avatar30,
+  // avatar31,
+  // avatar32,
+  // avatar33,
+  // avatar34,
 ];
