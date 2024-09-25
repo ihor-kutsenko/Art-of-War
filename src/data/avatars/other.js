09069e5cf709d38b70cf6c496a54a565189cb72c
Rammbest <igor-kutsenko@ukr.net>
2024-09-25 20:38:44 +0300

    update avatars other

--- src/data/avatars/other.js
+++ src/data/avatars/other.js
@@ -1,21 +1,31 @@
-import avatar1 from '../../images/avatars/other/1.png';
-import avatar2 from '../../images/avatars/other/2.png';
-import avatar3 from '../../images/avatars/other/3.png';
-import avatar4 from '../../images/avatars/other/4.png';
-import avatar5 from '../../images/avatars/other/5.png';
-import avatar6 from '../../images/avatars/other/6.png';
-import avatar7 from '../../images/avatars/other/7.png';
-import avatar8 from '../../images/avatars/other/8.png';
-import avatar9 from '../../images/avatars/other/9.png';
+// import avatar1 from '../../images/avatars/other/1.png';
+// import avatar2 from '../../images/avatars/other/2.png';
+// import avatar3 from '../../images/avatars/other/3.png';
+// import avatar4 from '../../images/avatars/other/4.png';
+// import avatar5 from '../../images/avatars/other/5.png';
+// import avatar6 from '../../images/avatars/other/6.png';
+// import avatar7 from '../../images/avatars/other/7.png';
+// import avatar8 from '../../images/avatars/other/8.png';
+// import avatar9 from '../../images/avatars/other/9.png';
 
 export const avatarsOther = [
-  avatar1,
-  avatar2,
-  avatar3,
-  avatar4,
-  avatar5,
-  avatar6,
-  avatar7,
-  avatar8,
-  avatar9,
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285453/1_ipabpk.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285453/2_ef5ebi.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285454/3_cnda2m.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285455/4_ly4inn.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285456/5_ccxypl.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285457/6_gq3093.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285458/7_darols.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285460/8_uocu5g.webp',
+  'https://res.cloudinary.com/dsy8hantq/image/upload/v1727285459/9_ltrjel.webp',
+
+  // avatar1,
+  // avatar2,
+  // avatar3,
+  // avatar4,
+  // avatar5,
+  // avatar6,
+  // avatar7,
+  // avatar8,
+  // avatar9,
 ];
