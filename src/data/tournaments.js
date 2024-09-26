efbc92e26cff31a2182c5328b6e7825947d12ab4
Rammbest <igor-kutsenko@ukr.net>
2024-09-26 20:58:32 +0300

    transfer tournaments img to cloudinary

--- src/data/tournaments.js
+++ src/data/tournaments.js
@@ -1,25 +1,25 @@
-import allForTheFrontImage from '../images/tournaments/all-for-the-front.jpg';
-import assaultImage from '../images/tournaments/assault.jpg';
-import battleRoyaleImage from '../images/tournaments/battle-royale.jpg';
-import behindTheWallImage from '../images/tournaments/behind-the-wall.jpg';
-import corsairsImage from '../images/tournaments/corsairs.jpg';
-import crossfireImage from '../images/tournaments/crossfire.jpg';
-import flightRecorderImage from '../images/tournaments/flight-recoder.png';
-import birthdayImage from '../images/tournaments/birthday.jpg';
-import jokerImage from '../images/tournaments/joker.jpg';
-import judgmentDayImage from '../images/tournaments/judgment-day.jpg';
-import largeReservesImage from '../images/tournaments/large-reserves.jpg';
-import newYearImage from '../images/tournaments/battle-for-christmas-trees.jpg';
-import niceLootImage from '../images/tournaments/big-booty.jpg';
-import onlyByChanceImage from '../images/tournaments/only-by-chance.jpg';
-import pumpkinMadnessImage from '../images/tournaments/pumpkin-madness.jpg';
-import reliablePartnerImage from '../images/tournaments/reliable-partner.jpg';
-import nautilusImage from '../images/tournaments/nautilus.jpg';
-import totalControlImage from '../images/tournaments/total-control.jpg';
-import twoCitadelsImage from '../images/tournaments/two-citadels.jpg';
-import vanguardImage from '../images/tournaments/vanguard.jpg';
-import fifthAnniversaryImage from '../images/tournaments/5th-anniversary.jpg';
-import emergencyCall from '../images/tournaments/emergency-call.jpg';
+// import allForTheFrontImage from '../images/tournaments/all-for-the-front.jpg';
+// import assaultImage from '../images/tournaments/assault.jpg';
+// import battleRoyaleImage from '../images/tournaments/battle-royale.jpg';
+// import behindTheWallImage from '../images/tournaments/behind-the-wall.jpg';
+// import corsairsImage from '../images/tournaments/corsairs.jpg';
+// import crossfireImage from '../images/tournaments/crossfire.jpg';
+// import flightRecorderImage from '../images/tournaments/flight-recoder.png';
+// import birthdayImage from '../images/tournaments/birthday.jpg';
+// import jokerImage from '../images/tournaments/joker.jpg';
+// import judgmentDayImage from '../images/tournaments/judgment-day.jpg';
+// import largeReservesImage from '../images/tournaments/large-reserves.jpg';
+// import newYearImage from '../images/tournaments/battle-for-christmas-trees.jpg';
+// import niceLootImage from '../images/tournaments/big-booty.jpg';
+// import onlyByChanceImage from '../images/tournaments/only-by-chance.jpg';
+// import pumpkinMadnessImage from '../images/tournaments/pumpkin-madness.jpg';
+// import reliablePartnerImage from '../images/tournaments/reliable-partner.jpg';
+// import nautilusImage from '../images/tournaments/nautilus.jpg';
+// import totalControlImage from '../images/tournaments/total-control.jpg';
+// import twoCitadelsImage from '../images/tournaments/two-citadels.jpg';
+// import vanguardImage from '../images/tournaments/vanguard.jpg';
+// import fifthAnniversaryImage from '../images/tournaments/5th-anniversary.jpg';
+// import emergencyCall from '../images/tournaments/emergency-call.jpg';
 
 import { allTheFrontMedals } from './tournaments/allTheFront';
 import { assaultMedals } from './tournaments/assault';
@@ -47,133 +47,155 @@ import { fifthAnniversaryMedals } from './tournaments/fifthAnniversary';
 export const tournaments = [
   {
     title: 'All for the Front',
-    imageUrl: allForTheFrontImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349641/all-for-the-front_v3jmnj.webp',
     buttonText: 'Medals',
     medals: allTheFrontMedals,
   },
   {
     title: 'Assault',
-    imageUrl: assaultImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349641/assault_ki6tdu.webp',
     buttonText: 'Medals',
     medals: assaultMedals,
   },
   {
     title: 'Battle Royale',
-    imageUrl: battleRoyaleImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349641/battle-royale_qfhz9k.webp',
     buttonText: 'Medals',
     medals: battleRoyaleMedals,
   },
   {
     title: 'Behind the Wall',
-    imageUrl: behindTheWallImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349641/behind-the-wall_lsct3i.webp',
     buttonText: 'Medals',
     medals: behindTheWallMedals,
   },
   {
     title: 'Corsairs',
-    imageUrl: corsairsImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349614/corsairs_spixfs.webp',
     buttonText: 'Medals',
     medals: nautilusMedals,
   },
   {
     title: 'Crossfire',
-    imageUrl: crossfireImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349614/crossfire_xeiww4.webp',
     buttonText: 'Medals',
     medals: crossfireMedals,
   },
   {
     title: 'Emergency Call',
-    imageUrl: emergencyCall,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349614/emergency-call_nhb3dc.webp',
     buttonText: 'Medals',
     medals: [],
   },
   {
     title: 'Flight Recorder',
-    imageUrl: flightRecorderImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349615/flight-recoder_gwnlzj.webp',
     buttonText: 'Medals',
     medals: flightRecorderMedals,
   },
   {
     title: 'Game Birthday',
-    imageUrl: birthdayImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349614/birthday_fd6lcb.webp',
     buttonText: 'Medals',
     medals: [],
   },
   {
     title: 'Joker',
-    imageUrl: jokerImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349614/joker_ag5joe.webp',
     buttonText: 'Medals',
     medals: jokerMedals,
   },
   {
     title: 'Judgment Day',
-    imageUrl: judgmentDayImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349615/judgment-day_xxjqik.webp',
     buttonText: 'Medals',
     medals: [],
   },
   {
     title: 'Large Reserves',
-    imageUrl: largeReservesImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349615/large-reserves_vlgwgk.webp',
     buttonText: 'Medals',
     medals: largeReservesMedals,
   },
   {
     title: 'New Years Alarm',
-    imageUrl: newYearImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349641/battle-for-christmas-trees_b5lq99.webp',
     buttonText: 'Medals',
     medals: newYearMedals,
   },
   {
     title: 'Nice Loot',
-    imageUrl: niceLootImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349614/big-booty_dedg7r.webp',
     buttonText: 'Medals',
     medals: niceLootMedals,
   },
   {
     title: 'Only by Chance',
-    imageUrl: onlyByChanceImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349615/only-by-chance_qcdnci.webp',
     buttonText: 'Medals',
     medals: onlyByChanceMedals,
   },
   {
     title: 'Pumpkin Madness',
-    imageUrl: pumpkinMadnessImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349615/pumpkin-madness_yewxqg.webp',
     buttonText: 'Medals',
     medals: pumpkinMedals,
   },
   {
     title: 'Reliable Partner',
-    imageUrl: reliablePartnerImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349615/reliable-partner_ep7it2.webp',
     buttonText: 'Medals',
     medals: reliablePartnerMedals,
   },
   {
     title: 'Searching the Nautilus ',
-    imageUrl: nautilusImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349615/nautilus_rebcln.webp',
     buttonText: 'Medals',
     medals: nautilusMedals,
   },
   {
     title: 'Total Control',
-    imageUrl: totalControlImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349616/total-control_torbnu.webp',
     buttonText: 'Medals',
     medals: totalControlMedals,
   },
   {
     title: 'Two Citadels',
-    imageUrl: twoCitadelsImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349616/two-citadels_z3wztg.webp',
     buttonText: 'Medals',
     medals: twoCitadelsMedals,
   },
   {
     title: 'Vanguard',
-    imageUrl: vanguardImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349616/vanguard_rnmhnn.webp',
     buttonText: 'Medals',
     medals: vanguardMedals,
   },
   {
     title: '5th Anniversary',
-    imageUrl: fifthAnniversaryImage,
+    imageUrl:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727349624/5th-anniversary_xgspub.webp',
     buttonText: 'Medals',
     medals: fifthAnniversaryMedals,
   },
