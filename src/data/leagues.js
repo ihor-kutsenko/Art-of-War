cab339cf0a99ea2d52897a21f8d475c48a6e7d89
Rammbest <igor-kutsenko@ukr.net>
2024-09-27 10:27:58 +0300

    transfer leagues img to cloudinary

--- src/data/leagues.js
+++ src/data/leagues.js
@@ -1,15 +1,32 @@
-import steel from '../images/leagues/steel.png';
-import bronze from '../images/leagues/bronze.png';
-import silver from '../images/leagues/silver.png';
-import gold from '../images/leagues/gold.png';
-import diamond from '../images/leagues/diamond.png';
-import star from '../images/leagues/star.png';
-
 export const leagues = [
-  { name: 'Steel league', image: steel },
-  { name: 'Bronze league', image: bronze },
-  { name: 'Silver league', image: silver },
-  { name: 'Gold league', image: gold },
-  { name: 'Diamond league', image: diamond },
-  { name: 'Star league', image: star },
+  {
+    name: 'Steel league',
+    image:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421922/steel_qfa4k6.webp',
+  },
+  {
+    name: 'Bronze league',
+    image:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421923/bronze_wldyrs.webp',
+  },
+  {
+    name: 'Silver league',
+    image:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421921/silver_qisrks.webp',
+  },
+  {
+    name: 'Gold league',
+    image:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421921/gold_m89qjb.webp',
+  },
+  {
+    name: 'Diamond league',
+    image:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421923/diamond_hgx2br.webp',
+  },
+  {
+    name: 'Star league',
+    image:
+      'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421922/star_x6dscj.webp',
+  },
 ];
