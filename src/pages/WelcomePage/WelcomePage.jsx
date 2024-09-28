f84d6d46557201d389ea970ddb58233ef0974987
Rammbest <igor-kutsenko@ukr.net>
2024-09-28 12:54:43 +0300

    add text to main title

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -6,6 +6,12 @@ import styles from './WelcomePage.module.scss';
 const WelcomePage = () => {
   return (
     <div className={styles.title}>
+      <p>
+        This site is dedicated to the game Art Of War 3: Global Conflict -
+        real-time strategy with direct control in battle. Here you will find
+        information about maps, tournaments, blitz tournaments, medals, avatars,
+        maximum leveling of units, buildings and heroes for each rank.
+      </p>
       <img src={main} alt="art of war" />
       <YoutubeTrailer videoId="4AfmuU7vW08" />
     </div>
