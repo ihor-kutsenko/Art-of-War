146dbd7977b7f8069ef86649365346390b36732b
Rammbest <igor-kutsenko@ukr.net>
2024-09-29 15:46:24 +0300

    add LoadMoreBtn to tournaments

--- src/components/Tournament/Tournament.jsx
+++ src/components/Tournament/Tournament.jsx
@@ -1,25 +1,21 @@
 import { useState } from 'react';
 
-import { tournaments } from 'data/tournaments';
-import PaginationContainer from 'components/Pagination/PaginationContainer';
-import Pagination from 'components/Pagination/Pagination';
-import useItemsPerPage from 'components/Pagination/PaginationHooks';
 import Item from 'components/Item/Item';
 import ItemList from 'components/ItemList/ItemList';
+import LoadMoreBtn from 'components/LoadMoreBtn/LoadMoreBtn';
+
+import { tournaments } from 'data/tournaments';
 
 const Tournament = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = useItemsPerPage();
+  const [visibleItems, setVisibleItems] = useState(5);
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = tournaments.slice(indexOfFirstItem, indexOfLastItem);
-
-  const handlePageChange = newPage => {
-    setCurrentPage(newPage);
+  const handleLoadMore = () => {
+    setVisibleItems(prevState => prevState + 5);
   };
+
+  const currentItems = tournaments.slice(0, visibleItems);
   return (
-    <PaginationContainer>
+    <>
       <ItemList>
         {currentItems.map((tournament, index) => (
           <Item
@@ -31,14 +27,11 @@ const Tournament = () => {
             disabled={tournament.medals.length === 0}
           />
         ))}
-        <Pagination
-          itemsPerPage={itemsPerPage}
-          totalItems={tournaments.length}
-          currentPage={currentPage}
-          onPageChange={handlePageChange}
-        />
+        {visibleItems < tournaments.length && (
+          <LoadMoreBtn handleLoadMore={handleLoadMore} />
+        )}
       </ItemList>
-    </PaginationContainer>
+    </>
   );
 };
 
