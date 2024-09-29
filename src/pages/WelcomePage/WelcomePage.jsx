b341fabb45cfd680828d72fcfa3660775ba31c39
Rammbest <igor-kutsenko@ukr.net>
2024-09-29 14:38:24 +0300

    add background and text to main page

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,20 +1,31 @@
 import YoutubeTrailer from 'components/YoutubeTrailer/YoutubeTrailer';
 
-import main from '../../images/main.webp';
+import Container from 'components/Container/Container';
+
 import styles from './WelcomePage.module.scss';
 
 const WelcomePage = () => {
   return (
-    <div className={styles.title}>
-      <p>
-        This site is dedicated to the game Art Of War 3: Global Conflict -
-        real-time strategy with direct control in battle. Here you will find
-        information about maps, tournaments, blitz tournaments, medals, avatars,
-        maximum leveling of units, buildings and heroes for each rank.
-      </p>
-      <img src={main} alt="art of war" />
-      <YoutubeTrailer videoId="4AfmuU7vW08" />
-    </div>
+    <>
+      <Container>
+        <div className={styles.container}>
+          <div className={styles.background}>
+            <div className={styles.textOverlay}>
+              <p>
+                This site is dedicated to the game Art Of War 3: Global Conflict
+                - real-time strategy with direct control in battle. Here you
+                will find information about maps, tournaments, blitz
+                tournaments, medals, avatars, maximum leveling of units,
+                buildings and heroes for each rank.
+              </p>
+            </div>
+          </div>
+          <div className={styles.trailerWrapper}>
+            <YoutubeTrailer videoId="4AfmuU7vW08" />
+          </div>
+        </div>
+      </Container>
+    </>
   );
 };
 
