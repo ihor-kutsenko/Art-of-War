45c2d2a1169ece3496e6e071124dcdac289968f8
Rammbest <igor-kutsenko@ukr.net>
2024-10-06 12:12:35 +0300

    updte footer

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -6,16 +6,20 @@ import Container from 'components/Container/Container';
 import Loader from 'components/Loader/Loader';
 import Footer from 'components/Footer/Footer';
 
+import styles from './SharedLayout.module.scss';
+
 export const SharedLayout = () => {
   return (
     <Container>
-      <Header />
-      <main>
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
-      </main>
-      <Footer />
+      <div className={styles.wrapper}>
+        <Header />
+        <main className={styles.main}>
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </main>
+        <Footer />
+      </div>
     </Container>
   );
 };
