7861679be5d083a996d79a26615c6070c37e2edc
Rammbest <igor-kutsenko@ukr.net>
2024-10-06 11:52:17 +0300

    add Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,22 @@
-const Loader = () => {
-  return <div>Loading...</div>;
-};
+import { CirclesWithBar } from 'react-loader-spinner';
+import styles from './Loader.module.scss';
 
+const Loader = () => {
+  return (
+    <div className={styles.backdrop}>
+      <CirclesWithBar
+        height="100"
+        width="100"
+        color="#ffa800"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        outerCircleColor="#1850ab"
+        innerCircleColor="#474747"
+        barColor=""
+        ariaLabel="circles-with-bar-loading"
+      />
+    </div>
+  );
+};
 export default Loader;
