9066e856917644e39251f8bb6e81e72298dc38d0
Rammbest <igor-kutsenko@ukr.net>
2024-10-25 11:54:49 +0300

    add flag counter

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -22,6 +22,16 @@ const WelcomePage = () => {
           </div>
           <div className={styles.trailerWrapper}>
             <YoutubeTrailer videoId="4AfmuU7vW08" />
+          </div>{' '}
+          <div className={styles.counter}>
+            {' '}
+            <a href="https://info.flagcounter.com/bGPE">
+              <img
+                src="https://s01.flagcounter.com/countxl/bGPE/bg_808080/txt_000000/border_CCCCCC/columns_8/maxflags_200/viewers_0/labels_0/pageviews_1/flags_0/percent_0/"
+                alt="Flag Counter"
+                border="0"
+              />
+            </a>
           </div>
         </div>
       </Container>
