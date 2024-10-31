2cd8778fc741e05a5e318c6d6771a06bb1d11c04
Rammbest <igor-kutsenko@ukr.net>
2024-10-31 16:26:56 +0200

    add translate to leagues

--- src/components/Leagues/Leagues.jsx
+++ src/components/Leagues/Leagues.jsx
@@ -1,12 +1,16 @@
+import { useTranslation } from 'react-i18next';
+
 import { leagues } from 'data/leagues';
 import styles from '../Orders/Orders.module.scss';
 
 const Leagues = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
       {leagues.map((order, index) => (
         <div key={index} className={styles.item}>
-          <h3 className={styles.title}>{order.name}</h3>
+          <h3 className={styles.title}>{t(order.name)}</h3>
           <img src={order.image} alt={order.name} className={styles.img} />
         </div>
       ))}
--- src/data/leagues.js
+++ src/data/leagues.js
@@ -1,31 +1,31 @@
 export const leagues = [
   {
-    name: 'Steel league',
+    name: 'leagues.steel',
     image:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421922/steel_qfa4k6.webp',
   },
   {
-    name: 'Bronze league',
+    name: 'leagues.bronze',
     image:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421923/bronze_wldyrs.webp',
   },
   {
-    name: 'Silver league',
+    name: 'leagues.silver',
     image:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421921/silver_qisrks.webp',
   },
   {
-    name: 'Gold league',
+    name: 'leagues.gold',
     image:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421921/gold_m89qjb.webp',
   },
   {
-    name: 'Diamond league',
+    name: 'leagues.diamond',
     image:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421923/diamond_hgx2br.webp',
   },
   {
-    name: 'Star league',
+    name: 'leagues.star',
     image:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421922/star_x6dscj.webp',
   },
