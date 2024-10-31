ba5498cf0058861c25af8b37b60fe97c8eb4d5b0
Rammbest <igor-kutsenko@ukr.net>
2024-10-31 16:09:20 +0200

    add translate to skirmish

--- src/data/skirmish.js
+++ src/data/skirmish.js
@@ -2,72 +2,75 @@ import { aiMedals } from './skirmish/ai';
 
 export const skirmish = [
   {
-    title: 'AI',
+    title: 'skirmish.ai.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421064/skirmish_lvag8f.webp',
     buttonText: 'Medals',
     medals: aiMedals,
+    info: 'skirmish.ai.info',
   },
   {
-    title: 'Survial',
+    title: 'skirmish.survial.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421112/survial_j4qte2.webp',
     buttonText: 'Medals',
     medals: [],
-    info: "To live or to survive; that is the question... If you choose to live, an enemy will annihilate you! So you'd better think about surviving. Hopefully, this mindset may help you hold for a while longer. Fight as if it's the final battle of your entire life! Fight to the last breath as if you're a wounded beast! Prove everybody that you do worth something. Now is the time to stay alive!",
+    info: 'skirmish.survial.info',
   },
   {
-    title: 'Test of Strength',
+    title: 'skirmish.testOfStrength.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421111/test-of-strength_uqraun.webp',
     buttonText: 'Medals',
     medals: [],
-    info: 'This time, Joker wants to check your defensive skills. Your aim is to survive as long as you can. A tough battle awaits you, as Joker always has an ace up his sleeve. Show him that you have some trump cards, too. The test of strength begins!',
+    info: 'skirmish.testOfStrength.info',
   },
   {
-    title: 'Birthday Game',
+    title: 'skirmish.birthdayGame.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1728228349/birthday-game_a1dgs7.webp',
     buttonText: 'Medals',
     medals: [],
+    info: 'skirmish.birthdayGame.info',
   },
   {
-    title: 'Summer Heat',
+    title: 'skirmish.summerHeat.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1728225351/summer-heat_ykf48m.webp',
     buttonText: 'Medals',
     medals: [],
+    info: 'skirmish.summerHeat.info',
   },
   {
-    title: 'Pumpkin Hunt',
+    title: 'skirmish.pumpkinHunt.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421065/pumpkin-hunt_c6bhqn.webp',
     buttonText: 'Medals',
     medals: [],
-    info: 'This unassailable base creates big problems for us. Therefore, two commanders will take part in the coming assault. Only jointly coordinated actions will lead you to victory!',
+    info: 'skirmish.pumpkinHunt.info',
   },
   {
-    title: 'Syndicate Trap',
+    title: 'skirmish.syndicateTrap.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1728225584/syndicate-trap_ovvbqw.webp',
     buttonText: 'Medals',
     medals: [],
-    info: "Intelligence has identified a significant concentration of Data Cells in sector K-537. The enemy reached the area before us and established an airbase to maintain control. Take the initiative by capturing all the Data Cells to gain crucial information on the enemy's latest technologies. Be cautious of enemy air raids.",
+    info: 'skirmish.syndicateTrap.info',
   },
   {
-    title: 'Christmas Cargo',
+    title: 'skirmish.christmasCargo.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421064/christmas-cargo_lai5nw.webp',
     buttonText: 'Medals',
     medals: [],
-    info: "To celebrate Christmas and the New Year, we will need Christmas trees. We must take them out from under the enemy's nose.",
+    info: 'skirmish.christmasCargo.info',
   },
   {
-    title: 'Battle for the Christmas Trees',
+    title: 'skirmish.battleForTheChristmasTrees.name',
     imageUrl:
       'https://res.cloudinary.com/dsy8hantq/image/upload/v1727421111/battle-for-the-christmas-trees_m1lyg7.webp',
     buttonText: 'Medals',
     medals: [],
-    info: 'Our army is pretty exhausted from this endless conflict. To raise morale, we must organize a good celebration of Christmas and the New Year. Capture Christmas trees!',
+    info: 'skirmish.battleForTheChristmasTrees.info',
   },
 ];
